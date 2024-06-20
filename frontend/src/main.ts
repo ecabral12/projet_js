d131105ced5027fafbbd279a57feb95982c32e01
Elvis Cabral <ecabral@dotscreen.com>
2024-06-20 15:28:12 +0200

    fix font awesome

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -13,7 +13,7 @@ import 'vue-toast-notification/dist/theme-bootstrap.css';
 //import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 /* import specific icons */
-import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+//import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
 
 /* add icons to the library */
 //library.add(faUserSecret)
