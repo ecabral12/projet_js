5bff6dcb99f49c9e80f3bebb8f1bc7cfbd74d0ef
djah97123 <160595084+djah97123@users.noreply.github.com>
2024-06-26 00:25:54 +0200

    Update deploy_swarm.sh

--- deploy_swarm.sh
+++ deploy_swarm.sh
@@ -1,56 +1,65 @@
 #!/bin/bash
 
-# Fonction pour installer Docker sur une instance Multipass
+
 install_docker() {
     local instance_name=$1
     multipass exec "$instance_name" -- sudo apt-get update
-    multipass exec "$instance_name" -- sudo apt-get install -y docker.io
+    # multipass exec "$instance_name" -- sudo apt-get install docker.io
+
+    # Add Docker's official GPG key:
+    multipass exec "$instance_name" -- sudo apt-get update
+    multipass exec "$instance_name" -- sudo apt-get install ca-certificates curl
+    multipass exec "$instance_name" -- sudo install -m 0755 -d /etc/apt/keyrings
+    multipass exec "$instance_name" -- sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+    multipass exec "$instance_name" -- sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+    # Add the repository to Apt sources:
+    multipass exec "$instance_name" -- echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+    $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+    multipass exec "$instance_name" -- sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    multipass exec "$instance_name" -- sudo apt-get update
 }
 
-# Fonction pour rejoindre un cluster Docker Swarm
 join_swarm() {
     local instance_name=$1
     local join_command=$2
     multipass exec "$instance_name" -- $join_command
 }
 
-# Lancement des instances Multipass
 multipass launch --name master
 multipass launch --name worker1
 multipass launch --name worker2
 
-# Installation de Docker sur toutes les instances
+
 install_docker master
 install_docker worker1
 install_docker worker2
 
-# Initialisation du cluster Docker Swarm sur l'instance master
+
 SWARM_INIT_OUTPUT=$(multipass exec master -- sudo docker swarm init --advertise-addr $(multipass info master | grep IPv4 | awk '{print $2}'))
 
-# Récupération du token pour joindre le cluster
+
 JOIN_TOKEN=$(multipass exec master -- sudo docker swarm join-token worker -q)
 
-# Commande de join pour les workers
+
 JOIN_COMMAND="sudo docker swarm join --token $JOIN_TOKEN $(multipass info master | grep IPv4 | awk '{print $2}'):2377"
 
-# Joindre les workers au cluster
+
 join_swarm worker1 "$JOIN_COMMAND"
 join_swarm worker2 "$JOIN_COMMAND"
 
-# Clonage du repository GitHub du projet
+
 multipass exec master -- git clone https://github.com/ecabral12/projet_js.git
 
-# Démarrage d'un registry Docker sur l'instance master
+
 multipass exec master -- sudo docker service create --name registry -p 5000:5000 registry
 
-# Construction et push des images vers le registry local
-multipass exec master -- bash -c "cd projet_js/frontend && sudo docker image build -t 127.0.0.1:5000/frontend_image:latest -f Dockerfile ."
-multipass exec master -- bash -c "cd projet_js/backend && sudo docker image build -t 127.0.0.1:5000/backend_image:latest -f Dockerfile ."
-multipass exec master -- sudo docker image push 127.0.0.1:5000/frontend_image:latest
+
+multipass exec master -- bash -c "cd AWS-Project-FLMP/app/server && sudo docker image build -t 127.0.0.1:5000/server_image:latest -f Dockerfile ."
+multipass exec master -- bash -c "cd AWS-Project-FLMP/app/client && sudo docker image build -t 127.0.0.1:5000/client_image:latest -f Dockerfile ."
 multipass exec master -- sudo docker image push 127.0.0.1:5000/backend_image:latest
+multipass exec master -- sudo docker image push 127.0.0.1:5000/frontend_image:latest
 
-# Déploiement du stack Docker Compose sur Docker Swarm
-multipass exec master -- sudo docker stack deploy -c projet_js/docker-compose-swarm.yml projet_js_stack
-
-# Affichage des services déployés
-multipass exec master -- sudo docker stack services projet_js_stack
+multipass exec master -- sudo docker stack deploy -c docker-compose-swarm.yml mon_projet
+multipass exec master -- sudo docker stack services mon_projet
